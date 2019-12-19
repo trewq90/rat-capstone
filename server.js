eed823f37510225c6ca1a21335c1b126709b433b
devinlubis <lubisdevin@gmail.com>
2019-12-19 12:31:41 -0500

    herok

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 })
-app.listen(port);
+app.listen(process.env.PORT || 5000)
 
 console.log("server started");
